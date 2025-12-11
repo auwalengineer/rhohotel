a0f0cbbdf0867d9036c3a3633adfebdf5ded7936
auwal isiaku <engr.auwal@live.com>
2025-12-11 22:26:29 +0000

    added connections on guest

--- rhohotel/rhocom_hotel/doctype/hotel_guest/hotel_guest_dashboard.py
+++ rhohotel/rhocom_hotel/doctype/hotel_guest/hotel_guest_dashboard.py
@@ -1,24 +1,29 @@
-# from frappe import _
+from frappe import _
 
-# def get_data():
-#     return {
-#         'fieldname': 'hotel_guest_name',
-#         'transactions': [
-#             {
-#                 'label': _('Related'),
-#                 'items': ['Hotel Room Check In', 'Sales Invoice']
-#             }
-#         ],
-#         'charts': [
-#             {
-#                 'label': _('Total Nights Stayed'),
-#                 'type': 'number',
-#                 'method': 'rhohotel.api.get_total_nights_stayed'
-#             },
-#             {
-#                 'label': _('Lifetime Value'),
-#                 'type': 'number',
-#                 'method': 'rhohotel.api.get_guest_lifetime_value'
-#             }
-#         ]
-#     }
+def get_data():
+    return {
+        'fieldname': 'guest',   # default for doctypes that use guest
+        'non_standard_fieldnames': {
+            'Sales Invoice': 'customer',
+            'Payment Entry': 'party'   # or 'customer' if that's your custom field
+        },
+        'transactions': [
+            {
+                'label': _('Related'),
+                'items': ['Hotel Room Check In', 'Payment Entry', 'Sales Invoice']
+            }
+        ],
+        'charts': [
+            {
+                'label': _('Total Nights Stayed'),
+                'type': 'number',
+                'method': 'rhohotel.api.get_total_nights_stayed'
+            },
+            {
+                'label': _('Lifetime Value'),
+                'type': 'number',
+                'method': 'rhohotel.api.get_guest_lifetime_value'
+            }
+        ]
+    }
+
